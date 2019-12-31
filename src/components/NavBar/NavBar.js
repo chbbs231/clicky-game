bf06bf48256fa22f91bde4291bb1fd6066701a3d
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-12-31 00:44:06 -0800

    Fixed navbar flex issues

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const NavBar = props => {
   return (
     // --- Navbar ---
-    <nav className="navbar navbar-expand-md navbar-dark primary-color">
+    <nav className="navbar navbar-dark primary-color">
 
       {/* Navbar Brand */}
       <span className="navbar-brand">
         <img src="./assets/svg/click.svg" alt="icon" /> Clicky
       </span>
 
-      <span className="navbar-text white-text">
+      <span className="navbar-text text-white">
         Score: {props.score}
       </span>
 
