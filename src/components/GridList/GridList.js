f7dcc72bc72b6c839d2e7c57abdf77e87a7ece65
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-02 15:53:28 -0800

    Added comments

--- src/components/GridList/GridList.js
+++ src/components/GridList/GridList.js
@@ -21,12 +21,15 @@ class GridList extends React.Component {
 
   state = { cards }
 
+  // Helper function that suffles elements of cards state
   shuffleCards() {
+    // Create deep copy of cards state
     let [...tempArr] = this.state.cards,
       currentIndex = tempArr.length,
       tempValue,
       randomIndex
     
+    // Shuffle tempArr
     while (0 !== currentIndex) {
       randomIndex = Math.floor( Math.random() * currentIndex )
       currentIndex -= 1
@@ -36,19 +39,23 @@ class GridList extends React.Component {
       tempArr[randomIndex] = tempValue
     }
 
+    // Set state to shuffled array
     this.setState({ cards: tempArr })
 
   }
 
+  // Event listener for card click
   handleCardClick(event, index, image) {
-    console.log(`Card ${image} has been clicked!`)
+    // If the card was already clicked
     if (this.state.cards[index].clicked) {
+      // Alert the user that they lost
       swal({
         title: 'You Lost!',
         text: `You scored ${this.props.score} point${this.props.score !== 1 ? 's' : ''}!`,
         icon: 'error',
         button: 'Okay'
       })
+      // Reset cards state
       this.setState({ cards: [
         { image: './assets/images/1.png', clicked: false },
         { image: './assets/images/2.png', clicked: false },
@@ -63,11 +70,15 @@ class GridList extends React.Component {
         { image: './assets/images/11.png', clicked: false },
         { image: './assets/images/12.png', clicked: false },
       ]})
+      // Reset parent's score state using parent function
       this.props.resetScore()
     } else {
+      // Card has not been clicked already
+      // Set card clicked to true
       let [...tempArr] = this.state.cards
       tempArr[index].clicked = true
       this.setState({ cards: tempArr })
+      // Increment parent's score state using parent function
       this.props.incrementScore()
       this.shuffleCards()
     }
