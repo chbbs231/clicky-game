ed43c845a6904af59ca3c188570c90d2f14d160d
Nathanael Hauser <nathanaelhauser@gmail.com>
2020-01-14 14:02:30 -0800

    Updates

---
+++ precache-manifest.862671863557c643a75326af9d9654ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18c971bc1c964b4165f1f3f8e77e935",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e763d95249d4a013a175",
+    "url": "/clicky-game/static/css/main.3192699c.chunk.css"
+  },
+  {
+    "revision": "fc5593f93199b6aaaf46",
+    "url": "/clicky-game/static/js/2.0f874610.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.0f874610.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e763d95249d4a013a175",
+    "url": "/clicky-game/static/js/main.35f15da4.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
